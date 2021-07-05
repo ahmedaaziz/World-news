0a069eae35450a58f342527469a5c6b635ab5680
ahmedaaziz <ahmed0abdelaziz@gmail.com>
2021-07-05 07:14:20 +0200

    Add Heroku command #6

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/news-web"));
+app.use(express.static("./dist/NEWS-WEB"));
 
-app.get("/*", (req, res) => res.sendFile("index.html", { root: "src/" }));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/NEWS-WEB/index.html"));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
